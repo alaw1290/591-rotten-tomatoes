6a74a9b4b9104d2792f772e0f0d3d06afefd0523
alaw1290 <alaw@bu.edu>
2016-11-05 17:30:34 -0400

    began work on url scraper

---
+++ RTscrapers/urlscraper.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+from contextlib import contextmanager
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
