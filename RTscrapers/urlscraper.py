2d2329dfb441d5ad32450d5688687af20fc8837b
alaw1290 <alaw@bu.edu>
2016-11-06 14:10:01 -0500

    working clicker

--- RTscrapers/urlscraper.py
+++ RTscrapers/urlscraper.py
@@ -9,40 +9,32 @@ from selenium.webdriver.support import expected_conditions as EC
 
 base_url = 'https://www.rottentomatoes.com/browse/dvd-all/?services=amazon;amazon_prime;fandango_now;hbo_go;itunes;netflix_iw;vudu'
 
-base_link = 'https://www.rottentomatoes.com'
-
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36'
-
 def parse_movie_urls():
     '''Creates dictionary of all movies under Browse All of RT using div class = "mb-movie"
     	and storing it as {title:url}'''
     
     #create dict
     movie_list = {}
-    driver = webdriver.Chrome()
-    driver.get("https://www.rottentomatoes.com/browse/dvd-all/?services=amazon;amazon_prime;fandango_now;hbo_go;itunes;netflix_iw;vudu#")
-    elem = driver.find_element_by_xpath('//*[@id="show-more-btn"]/button')
-    while(elem.is_displayed()):
-        elem.click()
-        elem = driver.find_element_by_xpath('//*[@id="show-more-btn"]/button')
-##elem = driver.find_element_by_name("q")
-##elem.clear()
-##elem.send_keys("pycon")
-##elem.send_keys(Keys.RETURN)
-##assert "No results found." not in driver.page_source
 
-    driver.close()
-    #start selenium test
-    headers = {'User-Agent' : user_agent}
-    response = requests.get(base_url, headers=headers)
-    html = response.text.encode('utf-8')
-    soup = BeautifulSoup(html,"lxml")
-    return soup
+    #start selenium driver, route to url
+    driver = webdriver.Firefox()
+    driver.get(base_url)
+
+    #while show more is there, keep clicking it
+    elem = driver.find_element_by_xpath('//*[@id="show-more-btn"]/button')
+
+    for i in range(100):
+        currrentpage = driver.find_element_by_id("content-column")
+        elem.click()
+        WebDriverWait(driver, 10)
+    #driver.close()
+    
+  
 
 if __name__ == '__main__':
 	#run scraper and print completion time
 	print('Running')
 	start = time.time()
-	test = parse_movie_urls()
+	parse_movie_urls()
 	end = time.time() - start
 	print("Completed, time: " + str(end) + " secs")
