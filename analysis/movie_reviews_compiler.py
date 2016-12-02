1fd98063e9fb744c19879c8c3f190369afe2fb71
alaw1290 <alaw@bu.edu>
2016-12-02 02:21:31 -0500

    finished movie reviews compiler

--- analysis/movie_reviews_compiler.py
+++ analysis/movie_reviews_compiler.py
@@ -1,6 +1,4 @@
 import pickle
-import json
-import numpy as np
 
 path = '../data/'
 
@@ -9,30 +7,41 @@ def import_pickle():
 		- movie_critic: dict of movies and corresponding critics that thumbs up/down them
 		- critic_movie: dict of critics and corresponding movies that they thumbs up/down '''
 
-	data = pickle.load(open(path + 'movie_reviews.pkl', 'rb'))
+	try:
+		movie_critic = pickle.load(open(path + "movie_critic.pkl", "rb" ) )
+		critic_movie = pickle.load(open(path + "critic_movie.pkl", "rb" ) )
+		matrix = pickle.load(open(path + "matrix.pkl", "rb" ) )
+		movie_keys = pickle.load(open(path + "movie_keys.pkl", "rb" ) )
+		critic_keys = pickle.load(open(path + "critic_keys.pkl", "rb" ) )
 
-	movie_critic = {}
-	critic_movie = {}
+		return movie_critic, critic_movie, matrix, movie_keys, critic_keys
 
-	for m in data:
+	except FileNotFoundError:
+		data = pickle.load(open(path + 'movie_reviews.pkl', 'rb'))
 
-		critics = {}
-		for c in data[m]:
+		movie_critic = {}
+		critic_movie = {}
 
-			val = data[m][c]['IsFresh']
-			critics[c] = val
+		for m in data:
 
-			try:
-				critic_movie[c].update({m:val})
-			except KeyError:
-				critic_movie[c] = {m:val}
+			critics = {}
+			for c in data[m]:
 
-		movie_critic[m] = critics
+				val = data[m][c]['IsFresh']
+				critics[c] = val
 
-	return movie_critic, critic_movie
+				try:
+					critic_movie[c].update({m:val})
+				except KeyError:
+					critic_movie[c] = {m:val}
 
-def export_matrix(movie_critic,critic_movie):
-	'''Uses corresponding movie_critic and critic_movie to construct numpy matrix and critic/movie key indicies (note only about 0.481196028% full) '''
+			movie_critic[m] = critics
+
+		m,mk,ck = convert_matrix(movie_critic,critic_movie)
+		return movie_critic, critic_movie, m,mk,ck
+
+def convert_matrix(movie_critic,critic_movie):
+	'''Uses corresponding movie_critic and critic_movie to construct numpy matrix and critic/movie key indicies (note only about 0.777625423% very sparse) '''
 
 	movie_keys = [k for k in movie_critic]
 	critic_keys = [k for k in critic_movie]
@@ -43,9 +52,31 @@ def export_matrix(movie_critic,critic_movie):
 		row = []
 		for m in movie_keys:
 			try:
-				row.append(int(critic_movie[c][m]))
+				if(critic_movie[c][m]):
+					row.append(1)
+				else:
+					row.append(-1)
 			except KeyError:
 				row.append(0)
 		matrix.append(row)
 
-	return np.toarray(matrix),movie_keys,critic_keys
+	return matrix,movie_keys,critic_keys
+
+def export_pickle(movie_critic,critic_movie,matrix,movie_keys,critic_keys):
+	'''Export everything and store as pickle files in the data folder specified in path'''
+
+	pickle.dump(movie_critic, open(path +  "movie_critic.pkl", "wb" ) )
+	pickle.dump(critic_movie, open(path +  "critic_movie.pkl", "wb" ) )
+	pickle.dump(matrix, open(path +  "matrix.pkl", "wb" ) )
+	pickle.dump(movie_keys, open(path +  "movie_keys.pkl", "wb" ) )
+	pickle.dump(critic_keys, open(path +  "critic_keys.pkl", "wb" ) )
+
+def run():
+	a,b,c,d,e = import_pickle()
+	export_pickle(a,b,c,d,e)
+
+if __name__ == '__main__':
+	run()
+
+
+
